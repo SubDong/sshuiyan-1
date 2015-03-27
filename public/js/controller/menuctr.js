7e438f9d64e7510f891e5d123be6a545f696abf3
guochunyan <guochunyan@perfect-cn.cn>
2015-03-27 12:37:30 +0800

    [mystyle]页面修改

--- public/js/controller/menuctr.js
+++ public/js/controller/menuctr.js
@@ -3,30 +3,34 @@
  */
 app.controller('AccordionDemoCtrl', function ($scope) {
     $scope.oneAtATime = true;
-
-    $scope.groups = [
-        {
-            title: '趋向分析 ',
-            content: 'Dynamic Group Body - 1',
-            template: "<h3>Hello, Directive</h3>"
-
-
-        },
-        {
-            title: 'Dynamic Group Header - 2',
-            content: 'Dynamic Group Body - 2'
-        }
-    ];
-
     $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
     $scope.addItem = function () {
         var newItemNo = $scope.items.length + 1;
         $scope.items.push('Item ' + newItemNo);
     };
-    $scope.currentMenu = "menu1";
+    $scope.currentMenu = "menuFirst";
     $scope.selectMenu = function (menu) {
         $scope.currentMenu = menu;
     }
 
 });
+/*********nav-select*********/
+app.controller('ngSelect', function ($scope) {
+
+    var vm = $scope.vm = {};
+
+    //数组对象用来给ng-options遍历
+    vm.optionsData = [{
+        title : "www.perfect.com"
+    },{
+        title : "www.perfect.com"
+    },{
+        title : "www.perfect.com"
+    },{
+        title : "www.perfect.com"
+    }];
+
+})
+
+
