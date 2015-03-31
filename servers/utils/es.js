6eb0ae5d071b7aca6f2f1cb713649efdd486b210
XiaoWei <xiaowei@perfect-cn.cn>
2015-03-31 17:31:09 +0800

    [es.js] 还原

--- servers/utils/es.js
+++ servers/utils/es.js
@@ -2,13 +2,24 @@ var elasticsearch = require('elasticsearch')
 //var pro = require('process')
 
 var eshosts = process.argv.slice(2)
-var client = new elasticsearch.Client({
-    host:'http://192.168.1.120:19200',
-    log:'debug',
-    sniffOnStart: true,
-    sniffInterval: 30000,
-    keepAlive: true
-})
+var client;
+
+if (eshosts.length > 0) {
+    client = new elasticsearch.Client({
+        host: 'http://' + eshosts[0],
+        sniffOnStart: true,
+        sniffInterval: 30000,
+        keepAlive: true
+    })
+
+} else {
+    client = new elasticsearch.Client({
+        host: '182.92.227.79:19200',
+        sniffOnStart: true,
+        sniffInterval: 30000,
+        keepAlive: true
+    })
+}
 
 
 
