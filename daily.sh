f4cbdbcc765c935b788f9fc3fb8c8286856edaa6
YouSheng <vbzerocool@gmail.com>
2015-04-02 09:17:54 +0800

    [build] 修改每日编译的脚本

--- daily.sh
+++ daily.sh
@@ -6,5 +6,8 @@ cd $CMD_PATH
 git pull
 git checkout develop
 
-mkdir - p ../logs
-forever restart -l ../logs/sshuiyan.log -o ../logs/sshuiyan.out -e ../logs/sshuiyan.error app.js $1
+mkdir -p ../logs
+
+forever stopall
+
+forever start -l ../logs/sshuiyan.log -o ../logs/sshuiyan.out -e ../logs/sshuiyan.error app.js $1
