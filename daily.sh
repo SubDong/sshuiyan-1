3751f03c6372f7245153c91bb80dab4ceb0c5ffd
YouSheng <vbzerocool@gmail.com>
2015-05-07 16:16:16 +0800

    Update daily.sh

--- daily.sh
+++ daily.sh
@@ -6,8 +6,4 @@ cd $CMD_PATH
 git pull
 git checkout develop
 
-mkdir -p ../logs
-
-forever stopall
-
-forever start -l $CMD_PATH/../logs/sshuiyan.log -o $CMD_PATH/../logs/sshuiyan.out -e $CMD_PATH/../logs/sshuiyan.error app.js $1
+pm2 reload app.js
