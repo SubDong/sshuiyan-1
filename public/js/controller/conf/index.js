494de492aea330b09d3716e212a5be3633349a5a
icepros <icepros9527@gmail.com>
2015-05-27 10:17:56 +0800

    修改了权限管理的控制器代码

--- public/js/controller/conf/index.js
+++ public/js/controller/conf/index.js
@@ -1,6 +1,7 @@
 /**
  * Created by Fzk lwek on 2015/5/25.
  */
-define(["./adminmainctr","./count_pagechange","./count_eventchange","./count_adtrack","./count_childlist","./admin_root"], function() {
+define(["./adminmainctr","./count_pagechange","./count_eventchange","./count_adtrack","./count_childlist","./admin_root",
+    "./admin/counticon-ctrl", "./admin/root-ctrl", "./admin/reportsite-ctrl"], function() {
 
 });
