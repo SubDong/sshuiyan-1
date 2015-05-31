13d9b3a3212a6a1ca8232cde7f42358ce0ff0514
YouSheng <vbzerocool@gmail.com>
2015-05-31 14:31:22 +0800

    [conf mongo schema] 已经完成的配置schema 1. 网站列表 2. 网站统计设置->统计规则设置 3. 网站统计设置->子目录管理 4. 网站统计设置->时长转化目标 ( 此处应改为 转化目标)

--- servers/db/schemas.js
+++ servers/db/schemas.js
@@ -1,7 +1,56 @@
-var hosts = {
-    site_id: String,
-    site_url: String,
-    site_name: String,
-    code_status : String
+// schema types 参考 http://mongoosejs.com/docs/schematypes.html
 
+var sites = {
+    id: String, // mongoid
+    uid: String, // user id
+    site_url: String, // site url
+    site_name: String, // site name
+    track_code: String, // js track id
+    track_status: String, // track code status
+    status: String, // enable or disable track
+    type_id : String // es type id ( hidden in front-end)
 }
+
+// 站点统计规则
+var siterules = {
+    id: String,
+    uid : String,
+    site_id: String,
+    // 统一URL, 数组
+    rules: [
+        {
+            source: String, // 源url
+            convert: String // 转换url
+        }
+    ],
+
+    ex_ips: [String],  // 排除IP
+    ex_refer_urls: [String], // 排除来源网站
+    ex_urls: [String], // 排除受访地址
+    cross_sites: [String] // 跨域监控
+}
+
+// 转化目标
+var converts = {
+    id: String,
+    uid: String,
+    time_conv: {
+        status: Boolean,
+        val: Number
+    },
+    pv_conv: {
+        status: Boolean,
+        val: Number
+    }
+}
+
+var subpaths = {
+    id: String,
+    uid: String,
+    site_id:String, // 站点ID
+    reg_enable: Boolean,
+    name: String,
+    paths: [String],
+    ex_paths: [String]
+}
+
