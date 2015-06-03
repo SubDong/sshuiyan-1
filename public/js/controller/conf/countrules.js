f5f698778274d9c5086be20511020895cc1ab3f4
liwenkaifzk <878679630@qq.com>
2015-06-03 11:16:12 +0800

    后台-统计规则页面js添加

--- public/js/controller/conf/countrules.js
+++ public/js/controller/conf/countrules.js
@@ -75,5 +75,54 @@ define(["./module"], function (ctrs) {
         $scope.addRule = function () {
             $scope.rules.push({source:"",convert:""});
         };
+        //点击效果
+        $scope.obj = {
+            "tNum": "1",
+            "tText": "",
+            "count": 1,
+            "helpFlag": false
+        };
+        $scope.ipObj = angular.copy($scope.obj);
+        $scope.tyymdzObj = angular.copy($scope.obj);
+        $scope.pclywzObj =angular.copy($scope.obj);
+        $scope.pcsfwzobj =angular.copy($scope.obj);
+        $scope.gyjkobj =angular.copy($scope.obj);
+        $scope.qdcxzhobj =angular.copy($scope.obj);
+        $scope.ips = [];
+        $scope.init = function () {
+            var ips = new Array(20);
+            for (var i = 0; i < ips.length; i++) {
+                ips[i] = {
+                    "id": ""
+                };
+                $scope.ips.push(ips[i]);
+            }
+        };
+        $scope.init();
+        $scope.myAdd = function (o) {
+            o.push({});
+        };
+
+        $scope.aaaaaaaaaaaaa = function (e, obj) {
+            var f = e.currentTarget;
+            var d = f.value.replace(/\r/gi, "");
+            var s = d.split("\n").length;
+            var num = "";
+            for (var i = 0; i < s; i++) {
+                num += (i + 1) + "\r\n";
+            }
+            obj.count = s;
+            obj.tNum = num;
+            obj.tText = f.value;
+            $(f.previousElementSibling).scrollTop(f.scrollTop);
+        };
+
+        $scope.myFocus = function (obj) {
+            obj.helpFlag = true;
+        };
+
+        $scope.myBlur = function (obj) {
+            obj.helpFlag = false;
+        };
     });
 });
