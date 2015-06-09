b24f790069ebe5858dbb23d30b124bf14fc77596
dengchanglu <dengchanglu_ss@163.com>
2015-06-09 16:53:38 +0800

    流量地图：时间按钮

--- public/js/controller/value/exchangectr.js
+++ public/js/controller/value/exchangectr.js
@@ -7,8 +7,8 @@ define(["./module"], function (ctrs) {
 
     ctrs.controller('exchangectr', function ($cookieStore, $http, $rootScope, $scope) {
             $scope.selectedIndex = 0;
-            $scope.start = -1;//时间偏移量开始
-            $scope.end = -1;//时间偏移量结束
+            $rootScope.start = -1;//时间偏移量开始
+            $rootScope.end = -1;//时间偏移量结束
             $scope.sites = [];
             $scope.exchange = {};
             //对应域名的点击时间，获取该域名下的层级下数据
@@ -33,20 +33,17 @@ define(["./module"], function (ctrs) {
                 });
                 $scope.selectedIndex = $index;
             };
-
-            //获取前天统计数据
-            $scope.beforeyesterday = function () {
-                $scope.start = -2;
-                $scope.end = -2;
-                $scope.init();
-            }
-            //获取昨天统计数据
-            $scope.timechange = function ($index) {
-                $scope.start = -1;
-                $scope.end = -1;
-                $scope.init();
-            }
-
+            $scope.reloadByCalendar = function (type) {
+                $rootScope.$broadcast("ssh_refresh_charts");
+                $rootScope.$broadcast("ssh_dateShow_options_time_change", type);
+            };
+            $scope.reset = function () {
+                $scope.todayClass = false;
+                $scope.yesterdayClass = false;
+                $scope.sevenDayClass = false;
+                $scope.monthClass = false;
+                $scope.definClass = false;
+            };
 
             $scope.usites = $cookieStore.get('usites');
 
@@ -64,7 +61,7 @@ define(["./module"], function (ctrs) {
             //根据域名type查询pv和uv
 
             $scope.init = function () {
-                $http.get("api/exchange?start=" + $scope.start + ",end=" + $scope.end + ",type=" + ids.substring(0, ids.length - 1)).success(function (data) {
+                $http.get("api/exchange?start=" + $rootScope.start + ",end=" + $rootScope.end + ",type=" + ids.substring(0, ids.length - 1)).success(function (data) {
                     $scope.exchanges = dataSave($scope, data);
                     $scope.exchange = {};
                     $scope.exchange = {
@@ -90,9 +87,125 @@ define(["./module"], function (ctrs) {
             $scope.isCollapsed = false;
             $scope.treeclose = true;
 
+            $scope.yesterday = function () {
+                $scope.reset();
+                $scope.isShowCalendar = false;
+                $scope.hiddenSeven = false;
+                $scope.todayCalendar = GetDateStr(-1);
+                $scope.hourselect = false;
+                $scope.dayselect = false;
+                $scope.weekselected = true;
+                $scope.mothselected = true;
+                $scope.lastDaySelect = true;
+                $scope.lastWeekSelect = true;
+                $scope.clearCompareSelect = true;
+                $rootScope.tableTimeStart = -1;
+                $rootScope.tableTimeEnd = -1;
+                $scope.reloadByCalendar("yesterday");
+                $('#reportrange span').html(GetDateStr(-1));
+                $scope.yesterdayClass = true;
+                $rootScope.start = -1;
+                $rootScope.end = -1;
+                $scope.init();
+            };
+            //获取前天统计数据
+            $scope.beforeyesterday = function () {
+                $scope.reset();
+                $scope.isShowCalendar = false;
+                $scope.hiddenSeven = false;
+                $scope.todayCalendar = GetDateStr(-2);
+                $scope.hourselect = false;
+                $scope.dayselect = false;
+                $scope.weekselected = true;
+                $scope.mothselected = true;
+                $scope.lastDaySelect = true;
+                $scope.lastWeekSelect = true;
+                $scope.clearCompareSelect = true;
+                $rootScope.tableTimeStart = -2;
+                $rootScope.tableTimeEnd = -2;
+                $scope.reloadByCalendar("beforeyesterday");
+                $('#reportrange span').html(GetDateStr(-2));
+                $scope.yesterdayClass = true;
+                $rootScope.start = -2;
+                $rootScope.end = -2;
+                $scope.init();
+            }
+            $scope.sevenDay = function () {
+                $scope.reset();
+                $scope.isShowCalendar = false;
+                $scope.hiddenSeven = true;//今日统计和昨日统计中，点击7、30天时隐藏对比
+                $scope.todayCalendar = GetDateStr(-6);
+                $scope.hourselect = false;
+                $scope.dayselect = false;
+                $scope.weekselected = true;
+                $scope.mothselected = true;
+                $rootScope.tableTimeStart = -7;
+                $rootScope.tableTimeEnd = -1;
+                $scope.reloadByCalendar("seven");
+                $('#reportrange span').html(GetDateStr(-6) + "至" + GetDateStr(0));
+                $scope.sevenDayClass = true;
+                $rootScope.start = -7;
+                $rootScope.end = -1;
+                $rootScope.interval = 7;
+                $scope.init();
+            };
+            $scope.month = function () {
+                $scope.reset();
+                $scope.isShowCalendar = false;
+                $scope.hiddenSeven = true;
+                $scope.hourselect = false;
+                $scope.dayselect = false;
+                $scope.weekselected = false;
+                $scope.mothselected = true;
+                $rootScope.tableTimeStart = -29;
+                $rootScope.tableTimeEnd = 0;
+                $scope.reloadByCalendar("month");
+                $('#reportrange span').html(GetDateStr(-29) + "至" + GetDateStr(0));
+                $scope.monthClass = true;
+                $rootScope.start = -30;
+                $rootScope.end = -1;
+                $rootScope.interval = 30;
+                $scope.init();
+            };
+            function GetDateStr(AddDayCount) {
+                var dd = new Date();
+                dd.setDate(dd.getDate() + AddDayCount);//获取AddDayCount天后的日期
+                var y = dd.getFullYear();
+                var m = dd.getMonth() + 1;//获取当前月份的日期
+                var d = dd.getDate();
+                return y + "-" + m + "-" + d;
+            }
+            $('#reportrange span').html(GetDateStr(0));
+            $('#reportrange').daterangepicker({
+                format: 'YYYY-MM-DD',
+                maxDate: GetDateStr(0),
+                showDropdowns: true,
+                showWeekNumbers: false,
+                timePicker: false,
+                //timePickerIncrement: 1,
+                timePicker12Hour: false,
+                opens: 'left',
+                drops: 'down',
+                timeZone: true,
+                buttonClasses: ['btn', 'btn-sm'],
+                applyClass: 'btn-primary',
+                cancelClass: 'btn-default',
+                separator: ' to '
+            }, function (start, end, label) {
+                $rootScope.datepickerClick(start.format('YYYY-MM-DD'), end.format('YYYY-MM-DD'), label);
+                $rootScope.startString = (start.format('YYYY-MM-DD') + ' 至 ' + end.format('YYYY-MM-DD'))
+                $('#reportrange span').html(start.format('YYYY-MM-DD') + '至' + end.format('YYYY-MM-DD'));
+            });
+            //日历
+            this.selectedDates = [new Date().setHours(0, 0, 0, 0)];
+            this.type = 'range';
+            /*      this.identity = angular.identity;*/
+
+            this.removeFromSelected = function (dt) {
+                this.selectedDates.splice(this.selectedDates.indexOf(dt), 1);
+            }
         }
     );
-
 });
 var dataSave = function ($scope, data) {
     var text = [];
