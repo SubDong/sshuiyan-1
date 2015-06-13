c9912859a63ac9427ccd266f6fa92baa2f518ee5
TomDing729 <327523994@qq.com>
2015-06-13 18:53:42 +0800

    后台配置- 事件转化目标-初始版本

--- public/js/controller/conf/eventchange_addctr.js
+++ public/js/controller/conf/eventchange_addctr.js
@@ -4,36 +4,7 @@
 define(["./module"], function (ctrs) {
     "use strict";
 
-    ctrs.controller('eventchange_addctr', function ($scope, $http,$rootScope,$cookieStore,ngDialog) {
-
-
-        $scope.eventChange = {};
-
-        $scope.eventChange.event_id = "0";
-
-        $scope.eventChange.event_name ="";
-
-        $scope.eventChange.event_page ="";
-
-        $scope.eventChange.uid =  $cookieStore.get("uid");
-
-        $scope.eventChange.root_url =$rootScope.userType;
-
-        $scope.eventChange.create_date = new Date().Format("yyyy-MM-dd hh:mm:ss");
-
-        $scope.site_url='';
-       $scope.saveUrl = function() {
-           $("#iframe").attr("src",'https://www.'+$scope.site_url);
-           $(".modal_title p").html('事件目标预览URL:'+$scope.site_url);
-           $(".event_modal").fadeIn();
-       }
-        $scope.closed = function() {
-            $(".event_modal").fadeOut();
-
-        }
     ctrs.controller('eventchange_addctr', function ($scope, $http, $rootScope, $cookieStore, ngDialog, $state) {
-
-
 
 
         $scope.eventChange = {};
@@ -65,7 +36,7 @@ define(["./module"], function (ctrs) {
         $scope.onSaveEvent = function () {
 
             var entity = JSON.stringify($scope.eventChange);
-           var url = "/config/eventchnage_list?type=save&entity=" + entity;
+            var url = "/config/eventchnage_list?type=save&entity=" + entity;
             $http({
                 method: 'GET',
                 url: url
@@ -88,5 +59,4 @@ define(["./module"], function (ctrs) {
             });
         };
     })
-});
 });
