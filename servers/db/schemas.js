feeba76f594f3dfeb2f462a283a858b24d199478
iammybest <396187665@qq.com>
2015-06-14 17:57:57 +0800

    [modify]#171 网站列表编辑功能 状态修改为4   -1：异常 0：待测试 1：正常 其他：未知  默认为待测试

--- servers/db/schemas.js
+++ servers/db/schemas.js
@@ -45,7 +45,7 @@ var schemas = {
             site_url: String, // site url 设置的URL
             site_name: String, // site name 设置的URL
             site_pause: Boolean,//配置暂停 true：暂停 false：使用
-            track_status: String,// track code status
+            track_status: Number,// track code status
             //status: String, // enable or disable track
             is_top: Boolean
         }
