cdadbc5a7992a9b159500402807fd2efe4a2f9f6
SubDong <shuzichao@126.com>
2015-06-14 16:10:13 +0800

    [update] trachJS功能修改

--- track/index.js
+++ track/index.js
@@ -9,68 +9,45 @@ var router = require('express').Router(),
 router.get('/', function (req, resp) {
     var tid = req.query["tid"];
     redis.service().get('tsj:'.concat(tid), function (err, sitejson) {
+        if (err || sitejson == null) {
+            resp.end();
+            return;
+        }
         sitejson = JSON.parse(sitejson);
-        redis.service().get(sitejson.siteid + ':m:'.concat(sitejson.siteurl), function (err, moust) {
-            redis.service().get('tt:'.concat(tid), function (err, typeid) {
-                redis.service().get("t:" + sitejson.siteid, function (err, timejson) {
-                    redis.service().get("p:" + sitejson.siteid, function (err, pvjson) {
-                        moust = JSON.parse(moust);
-                        timejson = JSON.parse(timejson);
-                        pvjson = JSON.parse(pvjson);
-
-                        sitejson = {}
-
-                        if (err || sitejson == null) {
-                            resp.end();
-                            return;
+        var ref = req.header('Referer');
+        if (ref == undefined || ref == '') {
+            resp.end();
+            return;
+        } else {
+            ref = ref.slice(0, ref.indexOf('?'));
+            var config = {
+                "domain": sitejson.siteurl,
+                "open": sitejson.sitepause   //是否启用
+            }
+            var siteid = sitejson.siteid ? sitejson.siteid : "1";
+            var tasks = ['typeid', 'mouse', 'duration', 'visit', 'evt'];
+            async.eachSeries(tasks, function (item, cb) {
+                var url = (item == "typeid" ? item.concat(":",tid) : item == "mouse" ? siteid.concat(":", item, ":", ref) : item.concat(":",siteid));
+                redis.service().get(url, function (err, val) {
+                    if (val != null) {
+                        try{
+                            config[item] = JSON.parse(val);
+                        } catch (e){
+                            config[item] = val;
                         }
-
-                        var ref = req.header('Referer')
-
-                        if (ref == undefined || ref == '') {
-                            resp.end()
-                            return;
-                        } else {
-                            ref = ref.slice(0, ref.indexOf('?'));
-
-                            var config = {
-                                "tid": typeid, //typeid
-                                "domain": sitejson.siteurl,
-                                "mouse": moust.mouse_ckick,
-                                "open": sitejson.site_pause,   //是否启用
-                                "timeOpen": timejson == null ? false : timejson.ttpause, //是否开起时长转化
-                                "timeVal": timejson == null ? 30 : timejson.tttime,  //时长转化时间
-                                "pageOpen": pvjson == null ? false : pvjson.pvpause,  //访问页数转化是否开起
-                                "pageVal": pvjson == null ? 3 : pvjson.pvtimes ,  //访问页数值
-                                "evt": []
-                            }
-
-                            var siteid = sitejson.siteid ? sitejson.siteid : "1";
-
-                            var tasks = ['mouse', 'evt'];
-
-                            async.eachSeries(tasks, function (item, cb) {
-                                //console.log(item)
-                                redis.service().get(siteid.concat(":", item, ":", ref), function (err, val) {
-                                    if (val != null)
-                                        config[item] = val[item];
-                                    cb();
-                                })
-                            }, function (err) {
-                                if (err) {
-                                    resp.end();
-                                } else {
-                                    flush(config, resp);
-                                }
-                            })
-                        }
-                    })
+                    }
+                    cb();
                 })
+            }, function (err) {
+                if (err) {
+                    resp.end();
+                } else {
+                    flush(config, resp);
+                }
             })
-        });
+        }
     });
-
-})
+});
 
 function flush(config, resp) {
     fs.readFile('./track/t.js', function (err, data) {
