07cf961e3fa13a235040e037afcb7ccaef4c7457
iammybest <396187665@qq.com>
2015-07-03 15:48:55 +0800

    [add]#290 编码错误 修改

--- track/dqh/select_t.js
+++ track/dqh/select_t.js
@@ -6,16 +6,16 @@
 
         /**
          *  add
-         *  添加 heartBeat之后
-         *  引入select.js
+         *  娣诲姞 heartBeat涔嬪悗
+         *  寮曞叆select.js
          */
         getSelectJS:function(){
-            //ccz.ref 判断来源
+            //ccz.ref 鍒ゆ柇鏉ユ簮
             console.log("getSelectJS")
             var _c = h.I;
             var a = document.createElement("script");
             a.setAttribute("type", "text/javascript");
-            a.setAttribute("src","http://127.0.0.1:8001/t.js/select?tid="+ c.id);//tid=b6dccb905b3003f75e40f79cc6786200 为trackid
+            a.setAttribute("src","http://127.0.0.1:8001/t.js/select?tid="+ c.id);//tid=b6dccb905b3003f75e40f79cc6786200 涓簍rackid
             var f = document.getElementsByTagName("script")[0];
             f.parentNode.insertBefore(a, f);
         }
