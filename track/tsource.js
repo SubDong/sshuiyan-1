1e44bda80048f7c5815da0519eb348cd86dd207c
icepros <icepros9527@gmail.com>
2015-08-05 17:52:03 +0800

    [adtrack]指定广告追踪  统计  update

--- track/tsource.js
+++ track/tsource.js
@@ -648,14 +648,7 @@ if (config != undefined && !config.open) {
             var tk = loc.split("?")[1].split("&")[5].split("=")[0];
             var ll = loc.split("?")[1].split("&")[6].split("=")[0];
             var tt = loc.split("?")[1].split("&")[7].split("=")[0];
-            /*var adObj = {
-                tid: loc.split("?")[1].split("&")[5].split("=")[1],
-                rf: loc.split("?")[0].split("/")[2],
-                media: loc.split("?")[1].split("&")[0].split("=")[1],
-                cpna: loc.split("?")[1].split("&")[2].split("=")[1],
-                kwna: loc.split("?")[1].split("&")[3].split("=")[1],
-                crt: loc.split("?")[1].split("&")[4].split("=")[1]
-            };*/
+
             if (sr == "rf" && md == "media" && pl == "cpna" && kw == "kwna" && ci == "crt" && tk == "t" && ll == "atk" && tt == "tt") {
                 md.g.adtrack = 1;
                 h.b.sm();
