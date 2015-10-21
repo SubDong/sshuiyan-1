8debf836e34494771be22e0744332fc4a1b8a492
weims <449598723@qq.com>
2015-10-21 16:39:51 +0800

    [fixbug]#502

--- servers/services/changeList_request.js
+++ servers/services/changeList_request.js
@@ -102,7 +102,7 @@ var changeList_request = {
 
                 for (var i = 0; i < pathNameArray.length; i++) {
                     var _d_i = dataPathName.indexOf(pathNameArray[i]);
-                    var _c_d_i = dataPathName.indexOf(pathNameArray[i]);
+                    var _c_d_i = contrastDataPathName.indexOf(pathNameArray[i]);
                     var _t_o = {
                         pathName: pathNameArray[i] == "-" ? "直接输入网址或标签" : pathNameArray[i],
                         pv: 0,
