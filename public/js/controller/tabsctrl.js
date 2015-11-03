6dc14a37a571b079feb99c6e734f71c6858c231c
weims <449598723@qq.com>
2015-11-03 17:50:20 +0800

    [fixbug]修复BUG#564

--- public/js/controller/tabsctrl.js
+++ public/js/controller/tabsctrl.js
@@ -725,6 +725,7 @@ define(["app"], function (app) {
             $scope.setAreaFilter("全部");
         })
         $scope.setAreaFilter = function (area) {
+            $rootScope.areaFilter = area;
             $scope.areaSearch = area == "全部" ? "" : area;
             if (area == "北京" || area == "上海" || area == "广州") {
                 if ($scope.city.selected != undefined) {
@@ -1289,7 +1290,12 @@ define(["app"], function (app) {
                 }).success(function (data, status) {
                     $rootScope.$broadcast("LoadDateShowDataFinish", data);
                     if ($rootScope.tableSwitch.promotionSearch != undefined && $rootScope.tableSwitch.promotionSearch) {
-                        var url = SEM_API_URL + "/sem/report/account?a=" + user + "&b=" + baiduAccount + "&startOffset=" + $rootScope.tableTimeStart + "&endOffset=" + $rootScope.tableTimeEnd + "&device=-1"
+                        if ($rootScope.areaFilter != "全部" && $location.path() == "/extension/way") {// 推广方式地域过滤不为全部是特殊处理
+                            var url = SEM_API_URL + "/sem/report/region?a=" + user + "&b=" + baiduAccount + "&startOffset=" + $rootScope.tableTimeStart + "&endOffset=" + $rootScope.tableTimeEnd + "&device=-1&rgna=" + $rootScope.areaFilter
+                        } else {
+                            var url = SEM_API_URL + "/sem/report/account?a=" + user + "&b=" + baiduAccount + "&startOffset=" + $rootScope.tableTimeStart + "&endOffset=" + $rootScope.tableTimeEnd + "&device=-1"
+                        }
+
                         $http({
                             method: 'GET',
                             url: url
@@ -1312,7 +1318,11 @@ define(["app"], function (app) {
                             $rootScope.checkedArray.forEach(function (item, i) {
                                 if ($rootScope.tableSwitch.latitude.field == "accountName") {
                                     if (dataSEM[0]) {
-                                        dataObj["accountName"] = "搜索推广 (" + dataSEM[0].accountName + ")";
+                                        if ($rootScope.areaFilter != "全部" && $location.path() == "/extension/way") {// 推广方式地域过滤不为全部是特殊处理
+                                            dataObj["accountName"] = "搜索推广 (" + dataSEM[0].regionName + ")";
+                                        }else {
+                                            dataObj["accountName"] = "搜索推广 (" + dataSEM[0].accountName + ")";
+                                        }
                                     } else {
                                         dataObj["accountName"] = "搜索推广 (暂无数据 )";
                                     }
