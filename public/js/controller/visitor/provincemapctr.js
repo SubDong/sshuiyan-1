a18dd54a95497f44432ce428842cf7d25f4ccf1e
TomDing729 <327523994@qq.com>
2015-11-10 17:58:51 +0800

    【访客分析】增加访客地图模块的邮件发送功能。#658

--- public/js/controller/visitor/provincemapctr.js
+++ public/js/controller/visitor/provincemapctr.js
@@ -2,7 +2,7 @@
  * Created by john on 2015/3/31.
  */
 define(["./module"], function (ctrs) {
-    ctrs.controller("provincemapctr", function ($scope, uiGridConstants, $rootScope, $http, areaService, $location, $stateParams, $state) {
+    ctrs.controller("provincemapctr", function ($scope, uiGridConstants, $rootScope, $http, $cookieStore,areaService, $location, $stateParams, $state) {
         $scope.allBrowsers = angular.copy($rootScope.browsers);
         //        高级搜索提示
         $scope.sourceSearch = "";
@@ -358,6 +358,69 @@ define(["./module"], function (ctrs) {
                 }
             };
             cb(docDefinition);
+        };
+
+
+        //发送邮件功能-初始化数据
+        $rootScope.initMailData = function () {
+            $http.get("api/saveMailConfig?rt=read&rule_url=" + $rootScope.mailUrl[14] + "").success(function (result) {
+                if (result) {
+                    var ele = $("ul[name='sen_form']");
+                    formUtils.rendererMailData(result, ele);
+                }
+            });
+        }
+        //发送邮件功能-确定发送
+        $scope.sendConfig = function () {
+            var formData = formUtils.vaildateSubmit($("ul[name='sen_form']"));
+            var result = formUtils.validateEmail(formData.mail_address, formData);
+            if (result.ec) {
+                alert(result.info);
+            } else {
+                formData.rule_url = $rootScope.mailUrl[14];
+                formData.uid = $cookieStore.get('uid');
+                formData.site_id = $rootScope.siteId;
+                formData.type_id = $rootScope.userType;
+                formData.schedule_date = $scope.mytime.time.Format('hh:mm');
+
+                $http.get("api/saveMailConfig?data=" + JSON.stringify(formData)).success(function (data) {
+                    var result = JSON.parse(eval("(" + data + ")").toString());
+                    if (result.ok == 1) {
+                        alert("操作成功!");
+                        $http.get("/api/initSchedule");
+                    } else {
+                        alert("操作失败!");
+                    }
+                });
+            }
+        };
+        //删除配置信息
+        $scope.deleteConfig = function () {
+            var formData = formUtils.vaildateSubmit($("ul[name='sen_form']"));
+            var result = formUtils.validateEmail(formData.mail_address, formData);
+            if (result.ec) {
+                alert(result.info);
+            } else {
+                formData.rule_url = $rootScope.mailUrl[14];
+                formData.uid = $cookieStore.get('uid');
+                formData.site_id = $rootScope.siteId;
+                formData.type_id = $rootScope.userType;
+
+                $http.get("api/deleteMailConfig?data=" + JSON.stringify(formData)).success(function (data) {
+                    var result = JSON.parse(eval("(" + data + ")").toString());
+                    if (result.ok == 1) {
+                        alert("操作成功!");
+                        $http.get("api/initSchedule").success(function (result) {
+                            if (result) {
+                                var ele = $("ul[name='sen_form']");
+                                formUtils.initData(ele);
+                            }
+                        });
+                    } else {
+                        alert("操作失败!");
+                    }
+                });
+            }
         };
 
 
