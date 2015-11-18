7e4fcdaaebc6165fff7bf6a03847a47f0f0e38a3
iammybest <396187665@qq.com>
2015-11-18 18:15:48 +0800

    [fixbug]#583 修正添加路劲时候默认两个步骤

--- public/js/controller/conf/pagechange_addctr.js
+++ public/js/controller/conf/pagechange_addctr.js
@@ -125,7 +125,9 @@ define(["./module"], function (ctrs) {
         };
         //增删路径
         $scope.addPaths = function (paths) {
-            paths.push(angular.copy(singlePath));
+            var temp =angular.copy(singlePath)
+            temp.steps.push(angular.copy(singleStep))
+            paths.push(temp);
         };
         $scope.removePath = function (paths, _index) {
             $scope.openPathConfirmDialog = ngDialog.openConfirm({
